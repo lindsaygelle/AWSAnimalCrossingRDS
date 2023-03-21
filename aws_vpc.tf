48fd5a01da2619a60eec0c661134493774f24276
lindsaygelle <lindsay.gelle@gmail.com>
2023-03-21 11:42:19 +1100

    Added remote state from AWSAnimalCrossingVPC

--- aws_vpc.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_vpc" "this" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    "region" = var.region
-    "Name"   = "animal-crossing"
-  }
-}
